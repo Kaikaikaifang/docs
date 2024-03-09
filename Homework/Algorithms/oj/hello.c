ec8001afe438f084ea78811acff08fc6e26537e3
kaikai[rasPi] <2837968358@qq.com>
2024-03-09 20:11:03 +0800

    feat: insert and reverse list

--- Homework/Algorithms/oj/hello.c
+++ Homework/Algorithms/oj/hello.c
@@ -1,3 +1,12 @@
+/**
+ * @project        : docs
+ * @file           : hello.c
+ * @dir            : ~/projects/docs/Homework/Algorithms/oj
+ * @date           : 2024/03/09
+ * @author         : Kaikai
+ * @brief          : hello, c!
+ * */
+
 #include <stdio.h>
 
 int main(void)
