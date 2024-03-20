32c5d918c04071f2f2909ba1dd0523e2e6ba5536
kaikai[rasPi] <2837968358@qq.com>
2024-03-20 15:36:22 +0800

    docs: oj2

--- Homework/Algorithms/oj/circular_queue.cpp
+++ Homework/Algorithms/oj/circular_queue.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 class CircularQueue {
 private:
-    int* arr;    // 存储队列元素的数组
+    int m;       // 数组长度
     int rear;    // 队尾指针
     int quelen;  // 队列当前长度
-    int m;       // 数组长度
+    int* arr;    // 存储队列元素的数组
 
 public:
     CircularQueue(int size) : m(size), rear(0), quelen(0) {
@@ -36,7 +36,7 @@ public:
     int dequeue() {
         if (isEmpty())
             throw std::underflow_error("Queue is empty");
-        int front = (rear - quelen + m) % m;
+        int front = (rear - quelen + m) % m;  // 计算队头指针
         int value = arr[front];
         quelen--;
         return value;
@@ -60,10 +60,11 @@ int main() {
     // 再次入队
     cq.enqueue(6);
     cq.enqueue(7);
-
+    
+    std::cout << "Is queue full? " << (cq.isFull() ? "Yes" : "No") << std::endl; // 应该输出 "Yes
     // 继续出队
     while (!cq.isEmpty()) {
-        std::cout << "Dequeue: " << cq.dequeue() << std::endl;
+        std::cout << "Dequeue: " << cq.dequeue() << std::endl;  // 应该输出 3 4 5 6 7
     }
 
     return 0;
